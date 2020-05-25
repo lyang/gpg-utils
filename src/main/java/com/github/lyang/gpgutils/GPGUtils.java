002bb939e73c23e23301c3992ab863bbd9f9e1da
Lin Yang <github@linyang.me>
2020-05-24 23:11:33 -0700

    Implement importKey

--- src/main/java/com/github/lyang/gpgutils/GPGUtils.java
+++ src/main/java/com/github/lyang/gpgutils/GPGUtils.java
@@ -1,4 +1,23 @@
 package com.github.lyang.gpgutils;
 
+import com.google.common.collect.ImmutableList;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GPGUtils {
+  private static final Logger LOGGER = LoggerFactory.getLogger(GPGUtils.class);
+
+  public static int importKey(File key, String... options)
+      throws IOException, InterruptedException {
+    return getGPGProcess("--import", options).redirectInput(key).start().waitFor();
+  }
+
+  private static ProcessBuilder getGPGProcess(String command, String... options) {
+    List<String> commands =
+        ImmutableList.<String>builder().add("gpg", command).add(options).build();
+    return new ProcessBuilder(commands).redirectError(ProcessBuilder.Redirect.INHERIT);
+  }
 }
--- src/test/java/com/github/lyang/gpgutils/GPGUtilsTest.java
+++ src/test/java/com/github/lyang/gpgutils/GPGUtilsTest.java
@@ -1,4 +1,32 @@
 package com.github.lyang.gpgutils;
 
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.io.Files;
+import com.google.common.io.MoreFiles;
+import com.google.common.io.Resources;
+import java.io.File;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class GPGUtilsTest {
+  private File tempDir;
+  private final File KEY = new File(Resources.getResource("private-key.gpg").getPath());
+
+  @Before
+  public void setUp() {
+    tempDir = Files.createTempDir();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    MoreFiles.deleteRecursively(tempDir.toPath());
+  }
+
+  @Test
+  public void importKey() throws IOException, InterruptedException {
+    assertEquals(0, GPGUtils.importKey(KEY, "--homedir", tempDir.getAbsolutePath()));
+  }
 }
