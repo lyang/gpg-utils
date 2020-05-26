95c902addb6934a99cab6cb0a3716ec01106da32
Lin Yang <github@linyang.me>
2020-05-25 22:48:54 -0700

    Add encryptStream

--- src/main/java/com/github/lyang/gpgutils/GPGUtils.java
+++ src/main/java/com/github/lyang/gpgutils/GPGUtils.java
@@ -1,23 +1,73 @@
 package com.github.lyang.gpgutils;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.io.ByteStreams;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GPGUtils {
   private static final Logger LOGGER = LoggerFactory.getLogger(GPGUtils.class);
+  private static final Consumer<InputStream> NOOP = stream -> {};
 
   public static int importKey(File key, String... options)
       throws IOException, InterruptedException {
-    return getGPGProcess("--import", options).redirectInput(key).start().waitFor();
+    return processStream("--import", new FileInputStream(key), NOOP, options);
+  }
+
+  public static int encryptStream(
+      InputStream stream, Consumer<InputStream> consumer, String... options)
+      throws IOException, InterruptedException {
+    return processStream("--encrypt", stream, consumer, options);
+  }
+
+  private static int processStream(
+      String command, InputStream stream, Consumer<InputStream> consumer, String... options)
+      throws IOException, InterruptedException {
+    Process process = getGPGProcess(command, options).start();
+    Thread writer = getWriterThread(process, stream);
+    Thread reader = getReaderThread(process, consumer);
+    writer.start();
+    reader.start();
+    writer.join();
+    reader.join();
+    return process.waitFor();
   }
 
   private static ProcessBuilder getGPGProcess(String command, String... options) {
     List<String> commands =
         ImmutableList.<String>builder().add("gpg", command).add(options).build();
     return new ProcessBuilder(commands).redirectError(ProcessBuilder.Redirect.INHERIT);
+  }
+
+  private static Thread getWriterThread(Process process, InputStream inputStream) {
+    return new Thread(
+        () -> {
+          try {
+            ByteStreams.copy(inputStream, process.getOutputStream());
+            process.getOutputStream().close();
+          } catch (IOException e) {
+            LOGGER.error("Failure in writer thread", e);
+            throw new RuntimeException(e);
+          }
+        });
+  }
+
+  private static Thread getReaderThread(Process process, Consumer<InputStream> consumer) {
+    return new Thread(
+        () -> {
+          try {
+            consumer.accept(process.getInputStream());
+            process.getInputStream().close();
+          } catch (IOException e) {
+            LOGGER.error("Failure in reader thread", e);
+            throw new RuntimeException(e);
+          }
+        });
   }
 }
--- src/test/java/com/github/lyang/gpgutils/GPGUtilsTest.java
+++ src/test/java/com/github/lyang/gpgutils/GPGUtilsTest.java
@@ -1,23 +1,33 @@
 package com.github.lyang.gpgutils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
 import com.google.common.io.MoreFiles;
 import com.google.common.io.Resources;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.function.Consumer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class GPGUtilsTest {
   private File tempDir;
+  private static final long BYTES = 1024 * 1024 * 100; // 100MB
   private final File KEY = new File(Resources.getResource("private-key.gpg").getPath());
+  private long outputLength = 0;
 
   @Before
-  public void setUp() {
+  public void setUp() throws IOException, InterruptedException {
     tempDir = Files.createTempDir();
+    outputLength = 0;
+    assertEquals(0, GPGUtils.importKey(KEY, "--homedir", tempDir.getAbsolutePath()));
   }
 
   @After
@@ -26,7 +36,35 @@ public class GPGUtilsTest {
   }
 
   @Test
-  public void importKey() throws IOException, InterruptedException {
-    assertEquals(0, GPGUtils.importKey(KEY, "--homedir", tempDir.getAbsolutePath()));
+  public void encryptStream() throws IOException, InterruptedException {
+    File file = new File(tempDir, getClass().getSimpleName() + ".txt");
+    assertEquals(0, generateRandomBytes(file, BYTES));
+    assertEquals(BYTES, file.length());
+    Consumer<InputStream> consumer =
+        stream -> {
+          try {
+            outputLength = ByteStreams.exhaust(stream);
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        };
+    assertEquals(
+        0,
+        GPGUtils.encryptStream(
+            new FileInputStream(file),
+            consumer,
+            "--homedir",
+            tempDir.getAbsolutePath(),
+            "--default-recipient-self"));
+    assertTrue(outputLength > 0);
+  }
+
+  private int generateRandomBytes(File file, long bytes) throws IOException, InterruptedException {
+    String command =
+        String.format("base64 /dev/urandom | head -c %d > %s", bytes, file.getAbsolutePath());
+    return new ProcessBuilder("/bin/sh", "-c", command)
+        .redirectError(Redirect.INHERIT)
+        .start()
+        .waitFor();
   }
 }
