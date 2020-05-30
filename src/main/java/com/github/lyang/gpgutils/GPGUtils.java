0a382f589d5e675b5d4678776a7da42631610c4c
Lin Yang <github@linyang.me>
2020-05-29 22:01:19 -0700

    Add Javadoc

--- src/main/java/com/github/lyang/gpgutils/GPGUtils.java
+++ src/main/java/com/github/lyang/gpgutils/GPGUtils.java
@@ -17,52 +17,120 @@ import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * {@link GPGUtils} is a simple wrapper for {@link ProcessBuilder} to make calling gpg easier in
+ * java.
+ */
 public class GPGUtils {
   private static final Logger LOGGER = LoggerFactory.getLogger(GPGUtils.class);
   private static final Consumer<InputStream> NOOP = stream -> {};
 
+  /**
+   * Import GPG public or private keys
+   *
+   * @param key The GPG key to be imported
+   * @param options CLI options to be passed to gpg {@link Process}
+   * @return Exit code of the gpg {@link Process}
+   */
   public static int importKey(File key, String... options)
       throws IOException, InterruptedException {
     return processStream("--import", new FileInputStream(key), NOOP, options);
   }
 
+  /**
+   * Encrypt a {@link String}
+   *
+   * @param input The {@link String} to be encrypted
+   * @param builder {@link StringBuilder} that will hold the armored result
+   * @param options CLI options to be passed to gpg {@link Process}
+   * @return Exit code of the gpg {@link Process}
+   */
   public static int encryptString(String input, StringBuilder builder, String... options)
       throws IOException, InterruptedException {
     return encryptStream(toInputStream(input), getWriter(builder), addArg("--armor", options));
   }
 
+  /**
+   * Encrypt a {@link File}
+   *
+   * @param input The {@link File} to be encrypted
+   * @param output The {@link File} to hold the encrypted result
+   * @param options CLI options to be passed to gpg {@link Process}
+   * @return Exit code of the gpg {@link Process}
+   */
   public static int encryptFile(File input, File output, String... options)
       throws IOException, InterruptedException {
     return encryptStream(new FileInputStream(input), getWriter(output), options);
   }
 
+  /**
+   * Encrypt an {@link InputStream}
+   *
+   * @param inputStream The {@link InputStream} to be encrypted
+   * @param consumer The {@link Consumer} for the encrypted input stream
+   * @param options CLI options to be passed to gpg {@link Process}
+   * @return Exit code of the gpg {@link Process}
+   */
   public static int encryptStream(
-      InputStream stream, Consumer<InputStream> consumer, String... options)
+      InputStream inputStream, Consumer<InputStream> consumer, String... options)
       throws IOException, InterruptedException {
-    return processStream("--encrypt", stream, consumer, options);
+    return processStream("--encrypt", inputStream, consumer, options);
   }
 
+  /**
+   * Decrypt a {@link String}
+   *
+   * @param input The {@link String} to be decrypted
+   * @param builder The {@link StringBuilder} to hold the decrypted result
+   * @param options CLI options to be passed to gpg {@link Process}
+   * @return Exit code of the gpg {@link Process}
+   */
   public static int decryptString(String input, StringBuilder builder, String... options)
       throws IOException, InterruptedException {
     return decryptStream(toInputStream(input), getWriter(builder), options);
   }
 
+  /**
+   * Decrypt a {@link File}
+   *
+   * @param input The {@link File} to be decrypted
+   * @param output The {@link File} to hold the decrypted result
+   * @param options CLI options to be passed to gpg {@link Process}
+   * @return Exit code of the gpg {@link Process}
+   */
   public static int decryptFile(File input, File output, String... options)
       throws IOException, InterruptedException {
     return decryptStream(new FileInputStream(input), getWriter(output), options);
   }
 
+  /**
+   * Decrypt an {@link InputStream}
+   *
+   * @param inputStream The {@link InputStream} to be decrypted
+   * @param consumer The {@link Consumer} for the decrypted input stream
+   * @param options CLI options to be passed to gpg {@link Process}
+   * @return Exit code of the gpg {@link Process}
+   */
   public static int decryptStream(
-      InputStream stream, Consumer<InputStream> consumer, String... options)
+      InputStream inputStream, Consumer<InputStream> consumer, String... options)
       throws IOException, InterruptedException {
-    return processStream("--decrypt", stream, consumer, options);
+    return processStream("--decrypt", inputStream, consumer, options);
   }
 
+  /**
+   * Process any gpg command against an {@link InputStream}
+   *
+   * @param command The gpg command
+   * @param inputStream The {@link InputStream} to be processed
+   * @param consumer The {@link Consumer} for the processed input stream
+   * @param options CLI options to be passed to gpg {@link Process}
+   * @return Exit code of the gpg {@link Process}
+   */
   public static int processStream(
-      String command, InputStream stream, Consumer<InputStream> consumer, String... options)
+      String command, InputStream inputStream, Consumer<InputStream> consumer, String... options)
       throws IOException, InterruptedException {
     Process process = getGPGProcess(command, options).start();
-    Thread writer = getWriterThread(process, stream);
+    Thread writer = getWriterThread(process, inputStream);
     Thread reader = getReaderThread(process, consumer);
     writer.start();
     reader.start();
